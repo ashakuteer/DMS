ac7befb1f189ad86c2660009c2e22d24a14b2aee
ashakuteer <88186701+ashakuteer@users.noreply.github.com>
2026-03-02 20:51:33 +0530

    Update donations.service.ts

--- apps/api/src/donations/donations.service.ts
+++ apps/api/src/donations/donations.service.ts
@@ -324,8 +324,8 @@ export class DonationsService {
               data.donorId,
               e164,
               "1": donorName,
-"2": donation.donationType || "General",
-"3": `${donation.currency} ${donation.donationAmount}`,
+          "2": donation.donationType || "General",
+          "3": `${donation.currency} ${donation.donationAmount},
               user.id,
             );
             communicationResults.whatsAppStatus = result.status;
