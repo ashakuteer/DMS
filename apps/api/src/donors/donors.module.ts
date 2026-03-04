3c56fc4c60ce8f9a026504c9b5cb83ea4ae02ba4
ashakuteer <88186701+ashakuteer@users.noreply.github.com>
2026-03-04 20:49:28 +0530

    Update donors.module.ts

--- apps/api/src/donors/donors.module.ts
+++ apps/api/src/donors/donors.module.ts
@@ -2,17 +2,31 @@ import { Module, forwardRef } from "@nestjs/common";
 import { DonorsController } from "./donors.controller";
 import { DonorsService } from "./donors.service";
 import { DonorDuplicatesService } from "./donor-duplicates.service";
+import { DonorsEngagementService } from "./donors.engagement.service";
+import { DonorsImportService } from "./donors.import.service";
+import { DonorsExportService } from "./donors.export.service";
+import { DonorsTimelineService } from "./donors.timeline.service";
+
 import { PrismaModule } from "../prisma/prisma.module";
 import { AuditModule } from "../audit/audit.module";
 import { StorageModule } from "../storage/storage.module";
 import { BeneficiariesModule } from "../beneficiaries/beneficiaries.module";
 
 @Module({
-  imports: [PrismaModule, AuditModule, StorageModule, forwardRef(() => BeneficiariesModule)],
+  imports: [
+    PrismaModule,
+    AuditModule,
+    StorageModule,
+    forwardRef(() => BeneficiariesModule),
+  ],
   controllers: [DonorsController],
   providers: [
     DonorsService,
     DonorDuplicatesService,
+    DonorsEngagementService,
+    DonorsImportService,
+    DonorsExportService,
+    DonorsTimelineService,
   ],
   exports: [
     DonorsService,
