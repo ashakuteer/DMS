273689544d3a9ba3531b800b67ff8ea249a506c4
ashakuteer <88186701+ashakuteer@users.noreply.github.com>
2026-03-04 21:17:38 +0530

    Update donors.engagement.service.ts

--- apps/api/src/donors/donors.engagement.service.ts
+++ apps/api/src/donors/donors.engagement.service.ts
@@ -1,1 +1,7 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
+@Injectable()
+export class DonorsEngagementService {
+  constructor(private prisma: PrismaService) {}
+}
