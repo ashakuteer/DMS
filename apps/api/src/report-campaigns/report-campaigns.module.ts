a5a752d66c2b5225b658c31b21e03ec153ddfcd0
ashakuteer <88186701+ashakuteer@users.noreply.github.com>
2026-03-06 19:51:38 +0530

    Update report-campaigns.module.ts

--- apps/api/src/report-campaigns/report-campaigns.module.ts
+++ apps/api/src/report-campaigns/report-campaigns.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { ReportCampaignsController } from './report-campaigns.controller';
 import { ReportCampaignsService } from './report-campaigns.service';
 import { PrismaModule } from '../prisma/prisma.module';
-import { EmailJobsModule } from '../email-jobs/email-jobs.module';
+import { StorageModule } from '../storage/storage.module'; // Add this line
 
 @Module({
-  imports: [PrismaModule, EmailJobsModule],
+  imports: [
+    PrismaModule,
+    StorageModule, // Add this line
+  ],
   controllers: [ReportCampaignsController],
   providers: [ReportCampaignsService],
+  exports: [ReportCampaignsService],
 })
 export class ReportCampaignsModule {}
