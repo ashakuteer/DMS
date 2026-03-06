722a3be3d76c1d138338c223474e6bc192905403
ashakuteer <88186701+ashakuteer@users.noreply.github.com>
2026-03-06 20:39:28 +0530

    Update report-campaigns.module.ts

--- apps/api/src/report-campaigns/report-campaigns.module.ts
+++ apps/api/src/report-campaigns/report-campaigns.module.ts
@@ -3,14 +3,14 @@ import { ReportCampaignsController } from './report-campaigns.controller';
 import { ReportCampaignsService } from './report-campaigns.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { StorageModule } from '../storage/storage.module';
-import { EmailJobsModule } from '../email-jobs/email-jobs.module';
+import { EmailJobsModule } from '../email-jobs/email-jobs.module'; // This line
 import { OrganizationProfileModule } from '../organization-profile/organization-profile.module';
 
 @Module({
   imports: [
     PrismaModule,
     StorageModule,
-    EmailJobsModule,
+    EmailJobsModule, // This line
     OrganizationProfileModule,
   ],
   controllers: [ReportCampaignsController],
