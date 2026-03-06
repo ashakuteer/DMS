c25bf9f786ceafc51cb7973ec8f41f70557721f7
ashakuteer <88186701+ashakuteer@users.noreply.github.com>
2026-03-06 18:58:12 +0530

    Update storage.module.ts

--- apps/api/src/storage/storage.module.ts
+++ apps/api/src/storage/storage.module.ts
@@ -1,7 +1,6 @@
-import { Module, Global } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { StorageService } from './storage.service';
 
-@Global()
 @Module({
   providers: [StorageService],
   exports: [StorageService],
