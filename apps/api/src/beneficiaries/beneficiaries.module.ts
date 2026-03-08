68b56ed625547b51f89211e1c4e2579646919fdf
ashakuteer <88186701+ashakuteer@users.noreply.github.com>
2026-03-08 20:00:50 +0530

    Update beneficiaries.module.ts

--- apps/api/src/beneficiaries/beneficiaries.module.ts
+++ apps/api/src/beneficiaries/beneficiaries.module.ts
@@ -1,17 +1,64 @@
 import { Module } from '@nestjs/common';
-import { BeneficiariesController, SponsorshipsController, BeneficiaryUpdatesController, SponsorDispatchesController, ReportCampaignsController } from './beneficiaries.controller';
+
+import {
+  BeneficiariesController,
+  SponsorshipsController,
+  BeneficiaryUpdatesController,
+  SponsorDispatchesController,
+  ReportCampaignsController
+} from './beneficiaries.controller';
+
 import { BeneficiariesService } from './beneficiaries.service';
 import { SponsorshipReminderScheduler } from './sponsorship-reminder.scheduler';
+
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuditModule } from '../audit/audit.module';
 import { EmailModule } from '../email/email.module';
 import { EmailJobsModule } from '../email-jobs/email-jobs.module';
 import { StorageModule } from '../storage/storage.module';
 
+import { BeneficiaryCoreService } from './services/beneficiary-core.service';
+import { BeneficiarySponsorshipService } from './services/beneficiary-sponsorship.service';
+import { BeneficiaryUpdatesService } from './services/beneficiary-updates.service';
+import { BeneficiaryHealthService } from './services/beneficiary-health.service';
+import { BeneficiaryEducationService } from './services/beneficiary-education.service';
+import { BeneficiaryDocumentsService } from './services/beneficiary-documents.service';
+import { BeneficiaryReportsService } from './services/beneficiary-reports.service';
+import { BeneficiaryRemindersService } from './services/beneficiary-reminders.service';
+
 @Module({
-  imports: [PrismaModule, AuditModule, EmailModule, EmailJobsModule, StorageModule],
-  controllers: [BeneficiariesController, SponsorshipsController, BeneficiaryUpdatesController, SponsorDispatchesController, ReportCampaignsController],
-  providers: [BeneficiariesService, SponsorshipReminderScheduler],
-  exports: [BeneficiariesService],
+  imports: [
+    PrismaModule,
+    AuditModule,
+    EmailModule,
+    EmailJobsModule,
+    StorageModule
+  ],
+
+  controllers: [
+    BeneficiariesController,
+    SponsorshipsController,
+    BeneficiaryUpdatesController,
+    SponsorDispatchesController,
+    ReportCampaignsController
+  ],
+
+  providers: [
+    BeneficiariesService,
+    SponsorshipReminderScheduler,
+
+    BeneficiaryCoreService,
+    BeneficiarySponsorshipService,
+    BeneficiaryUpdatesService,
+    BeneficiaryHealthService,
+    BeneficiaryEducationService,
+    BeneficiaryDocumentsService,
+    BeneficiaryReportsService,
+    BeneficiaryRemindersService
+  ],
+
+  exports: [
+    BeneficiariesService
+  ],
 })
 export class BeneficiariesModule {}
