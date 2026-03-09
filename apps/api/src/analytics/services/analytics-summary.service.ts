94bae938f67b675102cfb66c5d4ebeb8b4f0ef6c
ashakuteer <88186701+ashakuteer@users.noreply.github.com>
2026-03-09 23:48:55 +0530

    Update analytics-summary.service.ts

--- apps/api/src/analytics/services/analytics-summary.service.ts
+++ apps/api/src/analytics/services/analytics-summary.service.ts
@@ -1,100 +1,99 @@
-import { Injectable } from "@nestjs/common"
-import { PrismaService } from "../../prisma/prisma.service"
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../../prisma/prisma.service";
 import {
-  getMonthRange,
-  getTrailing12MonthsRange,
-} from "../utils/analytics-date.utils"
+getMonthRange,
+getTrailing12MonthsRange,
+} from "../utils/analytics-date.utils";
 
 @Injectable()
 export class AnalyticsSummaryService {
+constructor(private prisma: PrismaService) {}
 
-  constructor(private prisma: PrismaService) {}
+// simple memory cache with TTL
+private cache = new Map<
+string,
+{ data: any; expires: number }
 
-  // simple memory cache
-  private cache = new Map<string, any>()
+> ();
 
-  async getSummary() {
+async getSummary() {
+const cacheKey = "analytics_summary";
 
-    const cacheKey = "analytics_summary"
+```
+const cached = this.cache.get(cacheKey);
 
-    // return cached result if available
-    if (this.cache.has(cacheKey)) {
-      return this.cache.get(cacheKey)
-    }
+// return cached result if still valid
+if (cached && cached.expires > Date.now()) {
+  return cached.data;
+}
 
-    const { start: monthStart, end: monthEnd } = getMonthRange()
-    const { start: t12Start, end: t12End } = getTrailing12MonthsRange()
+const { start: monthStart, end: monthEnd } = getMonthRange();
+const { start: t12Start, end: t12End } = getTrailing12MonthsRange();
 
-    // run queries in parallel
-    const [
-      totalDonors,
-      donationsThisMonth,
-      donationsT12,
-      donationCountThisMonth,
-    ] = await Promise.all([
+// run queries in parallel
+const [
+  totalDonors,
+  donationsThisMonth,
+  donationsT12,
+  donationCountThisMonth,
+] = await Promise.all([
+  this.prisma.donor.count({
+    where: { deletedAt: null },
+  }),
 
-      this.prisma.donor.count({
-        where: { deletedAt: null },
-      }),
+  this.prisma.donation.aggregate({
+    _sum: { donationAmount: true },
+    where: {
+      deletedAt: null,
+      donationDate: {
+        gte: monthStart,
+        lte: monthEnd,
+      },
+    },
+  }),
 
-      this.prisma.donation.aggregate({
-        _sum: { donationAmount: true },
-        where: {
-          deletedAt: null,
-          donationDate: {
-            gte: monthStart,
-            lte: monthEnd,
-          },
-        },
-      }),
+  this.prisma.donation.aggregate({
+    _sum: { donationAmount: true },
+    where: {
+      deletedAt: null,
+      donationDate: {
+        gte: t12Start,
+        lte: t12End,
+      },
+    },
+  }),
 
-      this.prisma.donation.aggregate({
-        _sum: { donationAmount: true },
-        where: {
-          deletedAt: null,
-          donationDate: {
-            gte: t12Start,
-            lte: t12End,
-          },
-        },
-      }),
+  this.prisma.donation.count({
+    where: {
+      deletedAt: null,
+      donationDate: {
+        gte: monthStart,
+        lte: monthEnd,
+      },
+    },
+  }),
+]);
 
-      this.prisma.donation.count({
-        where: {
-          deletedAt: null,
-          donationDate: {
-            gte: monthStart,
-            lte: monthEnd,
-          },
-        },
-      }),
+const result = {
+  totalDonors,
 
-    ])
+  donationsThisMonth:
+    Number(donationsThisMonth._sum.donationAmount) || 0,
 
-    const result = {
-      totalDonors,
+  donationsT12:
+    Number(donationsT12._sum.donationAmount) || 0,
 
-      donationsThisMonth:
-        donationsThisMonth._sum.donationAmount
-          ? Number(donationsThisMonth._sum.donationAmount)
-          : 0,
+  donationCountThisMonth,
+};
 
-      donationsT12:
-        donationsT12._sum.donationAmount
-          ? Number(donationsT12._sum.donationAmount)
-          : 0,
+// store in cache (5 minutes)
+this.cache.set(cacheKey, {
+  data: result,
+  expires: Date.now() + 5 * 60 * 1000,
+});
 
-      donationCountThisMonth,
-    }
+return result;
+```
 
-    // store in cache
-    this.cache.set(cacheKey, result)
-
-    // auto clear cache after 5 minutes
-    setTimeout(() => {
-      this.cache.delete(cacheKey)
-    }, 300000)
-
-    return result
-  }
+}
 }
