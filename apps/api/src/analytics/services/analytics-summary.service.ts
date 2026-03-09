3d78500eaf8d6d380f18f5ca6b4e59ffa9d2145b
ashakuteer <88186701+ashakuteer@users.noreply.github.com>
2026-03-09 16:19:59 +0530

    Update analytics-summary.service.ts

--- apps/api/src/analytics/services/analytics-summary.service.ts
+++ apps/api/src/analytics/services/analytics-summary.service.ts
@@ -1,1 +1,57 @@
+import { Injectable } from "@nestjs/common"
+import { PrismaService } from "../../prisma/prisma.service"
+import {
+  getCurrentFY,
+  getMonthRange,
+  getTrailing12MonthsRange,
+} from "../utils/analytics-date.utils"
 
+@Injectable()
+export class AnalyticsSummaryService {
+  constructor(private prisma: PrismaService) {}
+
+  async getSummary() {
+    const { start: monthStart, end: monthEnd } = getMonthRange()
+    const { start: t12Start, end: t12End } = getTrailing12MonthsRange()
+
+    const [
+      totalDonors,
+      donationsThisMonth,
+      donationsT12,
+      donationCountThisMonth,
+    ] = await Promise.all([
+      this.prisma.donor.count({ where: { deletedAt: null } }),
+
+      this.prisma.donation.aggregate({
+        _sum: { donationAmount: true },
+        where: {
+          deletedAt: null,
+          donationDate: { gte: monthStart, lte: monthEnd },
+        },
+      }),
+
+      this.prisma.donation.aggregate({
+        _sum: { donationAmount: true },
+        where: {
+          deletedAt: null,
+          donationDate: { gte: t12Start, lte: t12End },
+        },
+      }),
+
+      this.prisma.donation.count({
+        where: {
+          deletedAt: null,
+          donationDate: { gte: monthStart, lte: monthEnd },
+        },
+      }),
+    ])
+
+    return {
+      totalDonors,
+      donationsThisMonth:
+        donationsThisMonth._sum.donationAmount?.toNumber() || 0,
+      donationsT12: donationsT12._sum.donationAmount?.toNumber() || 0,
+      donationCountThisMonth,
+    }
+  }
+}
